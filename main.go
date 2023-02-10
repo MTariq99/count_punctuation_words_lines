e6c07d3a62f48578787b8f4ea603e72777751814
muhammad Tariq <tariqwc99@gmail.com>
2023-02-10 14:41:03 +0500

    number of goroutines increased to optimize the execution time

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func CountingPunctuation(fileData []byte, channel chan result) {
 }
 
 func main() {
-	totalWords, totalPunc, totalLines := 0, 0, 0
+	// totalWords, totalPunc, totalLines := 0, 0, 0
 	file, err := os.Open("file.txt")
 	fileSize, _ := os.Stat("file.txt")
 	fileData := make([]byte, fileSize.Size())
@@ -51,32 +51,32 @@ func main() {
 
 	ch1 := make(chan result)
 	start := time.Now()
-	chunk := fileSize.Size() / 5
-	for i := 0; i < 5; i++ {
-		go CountingPunctuation(fileData[:chunk], ch1)
-		res1 := <-ch1
-		totalWords += res1.totalWords
-		totalPunc += res1.totalPunc
-		totalLines += res1.totalLines
-	}
-	fmt.Println("words----> ", totalWords, "\nPuncuations-----> ", totalPunc, "\nLines-----> ", totalLines, "\nBytes read------> ", fileBytes)
-	fmt.Println("execution time ", time.Since(start))
-
-	// go CountingPunctuation(fileData[:13360688], ch1)
-	// go CountingPunctuation(fileData[13360688:26721376], ch1)
-	// go CountingPunctuation(fileData[26721376:40082064], ch1)
-	// go CountingPunctuation(fileData[40082064:53442752], ch1)
-	// go CountingPunctuation(fileData[53442752:], ch1)
-
-	// res1 := <-ch1
-	// res2 := <-ch1
-	// res3 := <-ch1
-	// res4 := <-ch1
-	// res5 := <-ch1
-	// fmt.Println("\ntotal words ", res1.totalWords+res2.totalWords+res3.totalWords+res4.totalWords+res5.totalWords+1)
-	// fmt.Println("\ntotal punctuations ", res1.totalPunc+res2.totalPunc+res3.totalPunc+res4.totalPunc+res5.totalPunc)
-	// fmt.Println("\ntotal lines ", res1.totalLines+res2.totalLines+res3.totalLines+res4.totalLines+res5.totalLines)
-	// fmt.Println("\ntotal bytes used ", fileBytes)
+	// chunk := fileSize.Size() / 5
+	// for i := 0; i < 5; i++ {
+	// 	go CountingPunctuation(fileData[:chunk], ch1)
+	// 	res1 := <-ch1
+	// 	totalWords += res1.totalWords
+	// 	totalPunc += res1.totalPunc
+	// 	totalLines += res1.totalLines
+	// }
+	// fmt.Println("words----> ", totalWords, "\nPuncuations-----> ", totalPunc, "\nLines-----> ", totalLines, "\nBytes read------> ", fileBytes)
 	// fmt.Println("execution time ", time.Since(start))
+
+	go CountingPunctuation(fileData[:10688529], ch1)
+	go CountingPunctuation(fileData[10688529:21377058], ch1)
+	go CountingPunctuation(fileData[21377058:32065587], ch1)
+	go CountingPunctuation(fileData[32065587:42754116], ch1)
+	go CountingPunctuation(fileData[42754116:], ch1)
+
+	res1 := <-ch1
+	res2 := <-ch1
+	res3 := <-ch1
+	res4 := <-ch1
+	res5 := <-ch1
+	fmt.Println("\ntotal words ", res1.totalWords+res2.totalWords+res3.totalWords+res4.totalWords+res5.totalWords+1)
+	fmt.Println("\ntotal punctuations ", res1.totalPunc+res2.totalPunc+res3.totalPunc+res4.totalPunc+res5.totalPunc)
+	fmt.Println("\ntotal lines ", res1.totalLines+res2.totalLines+res3.totalLines+res4.totalLines+res5.totalLines+1)
+	fmt.Println("\ntotal bytes used ", fileBytes)
+	fmt.Println("execution time ", time.Since(start))
 
 }
